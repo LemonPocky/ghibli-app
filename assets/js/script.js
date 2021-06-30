14d4d9f976916b0965883c217572a25891e18d1d
LemonPocky <someone-70@live.com>
2021-06-29 18:50:10 -0700

    Added functions to help sort movies

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,9 @@ function init() {
 }
 
 function addExpandViewHandler() {
-    // TODO: .movie-title is a Placeholder
+    // TODO: .movie_details.html is a Placeholder
     const detailedPage = "movie_details.html";
-    // TODO: Placeholder
+    // TODO: .movie-title is a Placeholder
     movieListContainer.on("click", ".movie-title", function (event) {
         event.preventDefault();
         const id = $(this).attr("data-id");
@@ -57,6 +57,64 @@ function addToWatched(id) {
     watched.push(id);
     localStorage.setItem('watched', JSON.parse(watched));
     return watched;
+}
+
+function sortMovieList(sortType) {
+    let comparator;
+
+    // Change sort function depending on sortType
+    switch(sortType) {
+        case 'Alphabetical':
+            comparator = compareByAlphabetical;
+            break;
+        case 'Release Date':
+            comparator = compareByReleaseDate;
+            break;
+        case 'Favorites':
+            comparator = compareByFavorites;
+            break;
+        default:
+            comparator = compareByAlphabetical;
+    }
+
+    // Sort the list
+    movies = movies.sort(comparator);
+}
+
+// Helper function that compares two movies by alphabetical order of their titles
+function compareByAlphabetical(x, y) {
+    // TODO: title is a Placeholder
+    if (x.title < y.title) {
+        return -1;
+    }
+    if (x.title > y.title) {
+        return 1;
+    }
+    return 0;
+}
+
+// Helper function that compares two movies by their release dates
+function compareByReleaseDate(x, y) {
+    // TODO: releaseDate is a Placeholder
+    if (x.releaseDate < y.releaseDate) {
+        return -1;
+    }
+    if (x.releaseDate > y.releaseDate) {
+        return 1;
+    }
+    return 0;
+}
+
+// Helper function that compares two movies if they are Favorites
+function compareByFavorites(x, y) {
+    // TODO: favorite is a Placeholder
+    if (x.favorite && !y.favorite) {
+        return -1;
+    }
+    if (!x.favorite && y.favorite) {
+        return 1;
+    }
+    return 0;
 }
 
 init();
