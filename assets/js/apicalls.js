a028e56a2ffe73f93d2443bd2b2b5e093f6b5b9f
HibiAdelei <lilyadelame@gmail.com>
2021-07-01 19:42:40 -0700

    moved functions as requested

--- assets/js/apicalls.js
+++ assets/js/apicalls.js
@@ -3,55 +3,56 @@
 const omdbAPIkey = "5af38ba4";
 var test = document.getElementById('root');
 
-function initApi() {
+function fetchAllGhibliMovies() {
   apicall();
   buildOMDBApiCall();
   var request = new XMLHttpRequest();
+  var container = document.createElement('div');
+  container.setAttribute('class', 'container');
+  test.appendChild(container);
 
-  request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+    request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+    request.onload = function () {
+       // parsing json data
+     var data = JSON.parse(this.response);
+     if (request.status >= 200 && request.status < 400) {
 
-  request.onload = function () {
+        data.forEach(movie => {
+         var card = document.createElement('div');
+         card.setAttribute('class', 'card');
+         card.setAttribute("data-id", movie.id)
 
-   // parsing json data
-    var data = JSON.parse(this.response);
-    if (request.status >= 200 && request.status < 400) {
-
-      data.forEach(movie => {
-        var card = document.createElement('div');
-        card.setAttribute('class', 'card');
-        card.setAttribute("data-id", movie.id)
-
-        var h1 = document.createElement('h1');
-        h1.textContent = movie.title;
+         var h1 = document.createElement('h1');
+         h1.textContent = movie.title;
      
 
-        var p = document.createElement('p');
-        p.textContent = `${movie.description}`;
+         var p = document.createElement('p');
+         p.textContent = `${movie.description}`;
     
-        //   //todo: fix poster links
-        //   var img = document.createElement('img');
-        //   img.src = movie.poster;
-        container.appendChild(card);
-        card.appendChild(h1);
-        card.appendChild(p);
-        //   card.appendChild(img);  
-      });
+         //   //todo: fix poster links
+         //   var img = document.createElement('img');
+         //   img.src = movie.poster;
+         container.appendChild(card);
+         card.appendChild(h1);
+         card.appendChild(p);
+
+     
+         //   card.appendChild(img);  
+        });
           
-    } else {
-        //displays a marquee of text if there's a problem fetching the api data
-           var error = document.createElement('marquee');
-           error.textContent = `error retrieving data`;
-           test.appendChild(error);
-      }
-  } 
+     }   else {
+          //displays a marquee of text if there's a problem fetching the api data
+         var error = document.createElement('marquee');
+         error.textContent = `error retrieving data`;
+         test.appendChild(error);
+        }
+   } 
   request.send();
 }
 
-initApi();
+fetchAllGhibliMovies();
 
-var container = document.createElement('div');
-container.setAttribute('class', 'container');
-test.appendChild(container);
+
 
 
 function apicall() {
